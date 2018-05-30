8ce60ad19e8c049ed9e0ba00512535934f428816
Nathanaël Courant <nathanael.courant@inria.fr>
2018-05-30 15:44:24 +0200

    Notations for easier specifications

--- Extraction.v
+++ Extraction.v
@@ -4,7 +4,10 @@ Require Import Instr.
 Require Import Vpl.ImpureConfig.
 Require Import List.
 Require Import String.
+Require Import Linalg.
+Require Import ZArith.
 
+Open Scope Z_scope.
 Import ListNotations.
 
 Require Extraction.
@@ -19,12 +22,30 @@ Extraction Blacklist String List.
 Axiom dummy : instr.
 Extract Inlined Constant dummy => "0".
 
+Definition vr n := (assign n 1 nil, 0).
+Arguments vr n%nat.
+
+Delimit Scope aff_scope with aff.
+Notation "x * y" := (mult_constraint x y) : aff_scope.
+Local Arguments mult_constraint k%Z c%aff.
+
+Notation "x + y" := (add_constraint x y) : aff_scope.
+Notation "- x" := (mult_constraint (-1) x) : aff_scope.
+Notation "x - y" := (x + -y)%aff : aff_scope.
+Notation "$ x" := (nil, x) (at level 30) : aff_scope.
+
+Notation "x <= y" := (mult_constraint_cst (-1) (add_constraint x (mult_constraint (-1) y))) : aff_scope.
+Notation "x >= y" := (y <= x)%aff : aff_scope.
+
+Definition a := vr 0. Definition b := vr 1. Definition c := vr 2. Definition d := vr 3. Definition e := vr 4. Definition f := vr 5.
+
+(*
 Definition test_pi_1 := {|
    pi_instr := dummy ;
    pi_poly := [ ([0; 0; -1; 0], 0); ([0; 0; 0; -1], 0); ([-1; 0; 1; 0], 0); ([0; -1; 0; 1], 0) ] ;
    pi_schedule := [([0; 0; 0; 1], 0); ([0; 0; 1; 0], 0)] ;
    pi_transformation := [([0; 0; 1; 0], 0); ([0; 0; 0; 1], 0)] ;
-|}.
+|}%aff.
 
 Definition test_pi_2 := {|
    pi_instr := dummy ;
@@ -46,6 +67,38 @@ Definition test_pi_4 := {|
    pi_schedule := [([0; -3; 1], 0); ([0; 1; 0], 0)] ;
    pi_transformation := [([0; 0; 1], 0)] ;
 |}.
+*)
+
+Open Scope aff_scope.
+
+Definition test_pi_1 := {|
+   pi_instr := dummy ;
+   pi_poly := [ $0 <= c ; $0 <= d ; c <= a ; d <= b ] ;
+   pi_schedule := [d ; c] ;
+   pi_transformation := [c ; d] ;
+|}.
+
+Definition test_pi_2 := {|
+   pi_instr := dummy ;
+   pi_poly := [ $0 <= d ; d <= a ; b <= 3 * d ; 3 * d <= b ; c <= 2 * d ; 2 * d <= c ] ;
+   pi_schedule := [b ; c ; d] ;
+   pi_transformation := [b ; c ; d] ;
+|}.
+
+Definition test_pi_3 := {|
+   pi_instr := dummy ;
+   pi_poly := [ $0 <= c ; c <= a ; $0 <= c - 3 * b ; c - 3 * b <= $2 ] ;
+   pi_schedule := [b ; c - 3 * b] ;
+   pi_transformation := [c] ;
+|}%aff.
+
+Definition test_pi_4 := {|
+   pi_instr := dummy ;
+   pi_poly := [ $0 <= c ; c <= a ; $0 <= c - 3 * b ; c - 3 * b <= $2 ] ;
+   pi_schedule := [c - 3 * b ; b] ;
+   pi_transformation := [c] ;
+|}.
+
 
 Open Scope string_scope.
 
